f558ff2ceec6f1537daed17dad37e49409e3b8bb
Haustgeirr <3540179+Haustgeirr@users.noreply.github.com>
2021-03-02 14:55:06 +0000

    heroku

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,14 @@ const origin = {
 
 app.use(cors(origin));
 app.use('/api/v1', v1Router);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 // Set up listener
 const PORT = process.env.PORT || 5000;
